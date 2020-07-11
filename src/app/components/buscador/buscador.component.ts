fe81d3f63528c3b7c1fbf270961d20008c70dd74
Gus2708 <scarygamer08@gmail.com>
2020-07-11 00:40:09 -0400

    routing error

---
+++ src/app/components/buscador/buscador.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-buscador',
+  templateUrl: './buscador.component.html'
+})
+export class BuscadorComponent implements OnInit {
+  heroes: any[] = []
+  termino:string
+
+  constructor(private _activatedroute:ActivatedRoute,
+    private heroesservice:HeroesService,
+    private router:Router) { }
+  
+  ngOnInit(): void {
+    this._activatedroute.params.subscribe(params => {
+      this.heroes = this.heroesservice.buscarHeroe(params['termino']);
+      //console.log(this.heroes);
+      this.termino = params['temino'];
+    })
+  }
+  verHeroe( idx:number ){
+    this.router.navigate( ['/heroe',idx] );
+  }
+
+}
